7e4db6c3f94cda94a91f6a80d5f473e5144ba95c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-03-18 00:00:52 +0000

    simplify

--- tests/core/pyspec/eth_consensus_specs/utils/ssz/ssz_typing.py
+++ tests/core/pyspec/eth_consensus_specs/utils/ssz/ssz_typing.py
@@ -589,10 +589,10 @@ class Container(View):
         if annotations:
             cls._field_names = tuple(annotations.keys())
             cls._field_types = tuple(annotations.values())
+            cls._field_type_map = dict(zip(cls._field_names, cls._field_types))
 
     def __init__(self, *, backing=None, **kwargs):
         if backing is not None:
-            # Copy from a backing object (used by test caching)
             for name in self._field_names:
                 val = getattr(backing, name)
                 if hasattr(val, "copy") and callable(val.copy):
@@ -602,18 +602,15 @@ class Container(View):
         for name, typ in zip(self._field_names, self._field_types):
             value = kwargs.get(name)
             if value is None:
-                # Default value for the type
                 value = typ()
             elif not isinstance(value, typ):
                 value = typ(value)
             self.__dict__[name] = value
 
     def __setattr__(self, name, value):
-        if name in self._field_names:
-            idx = self._field_names.index(name)
-            typ = self._field_types[idx]
-            if not isinstance(value, typ):
-                value = typ(value)
+        typ = self._field_type_map.get(name)
+        if typ is not None and not isinstance(value, typ):
+            value = typ(value)
         self.__dict__[name] = value
 
     def __eq__(self, other):
@@ -738,6 +735,7 @@ class ProgressiveContainer(View, metaclass=_ProgressiveContainerMeta):
         if annotations:
             cls._field_names = tuple(annotations.keys())
             cls._field_types = tuple(annotations.values())
+            cls._field_type_map = dict(zip(cls._field_names, cls._field_types))
 
     def __init__(self, **kwargs):
         for name, typ in zip(self._field_names, self._field_types):
@@ -749,11 +747,9 @@ class ProgressiveContainer(View, metaclass=_ProgressiveContainerMeta):
             self.__dict__[name] = value
 
     def __setattr__(self, name, value):
-        if name in self._field_names:
-            idx = self._field_names.index(name)
-            typ = self._field_types[idx]
-            if not isinstance(value, typ):
-                value = typ(value)
+        typ = self._field_type_map.get(name)
+        if typ is not None and not isinstance(value, typ):
+            value = typ(value)
         self.__dict__[name] = value
 
     def __eq__(self, other):
